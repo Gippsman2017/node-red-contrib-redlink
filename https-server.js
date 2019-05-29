a894151ca6c6cc96237a7e1d29c4466b081ab360
umasudhan <umasudhan.p@gmail.com>
2019-05-29 15:16:51 +1000

    add express to https-server for easier route management

--- https-server.js
+++ https-server.js
@@ -2,18 +2,18 @@ const https = require('https');
 const selfsigned = require('selfsigned');
 const attrs = [{name: 'commonName', value: 'wombat.abcd.nbnco.com.au'}];
 const pems = selfsigned.generate(attrs, {days: 3650});
+const express = require('express');
+const app = express();
 let server;
-module.exports.startServer = function (port, callback) {
+module.exports.startServer = function (port) {
     try {
         server = https.createServer({
             key: pems.private,
             cert: pems.cert
-        }, (req, res) => { //server to listen on inbound messages- notify from producers; register from consumers/downstream branches
-            callback(req, res);
-            console.log(req.url);
-        }).listen(port);
+        }, app).listen(port);
+        return server;
     } catch (e) {
-        console.log(e); //todo robust error handling
+        console.log(e); //todo error handling
     }
 };
 
@@ -21,3 +21,6 @@ module.exports.getServer = function () {
     return server;
 };
 
+module.exports.getExpressApp = function () {
+    return app;
+};
