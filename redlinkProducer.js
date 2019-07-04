f16c5b053273f5734b4fe481518c8fd557b9e9a6
umasudhan <umasudhan.p@gmail.com>
2019-07-04 15:28:10 +1000

    refactor log function; rename msgid to redlinkMsgId to avoid confusion with node-red msgid;

--- redlinkProducer.js
+++ redlinkProducer.js
@@ -2,6 +2,7 @@ const alasql = require('alasql');
 
 const base64Helper = require('./base64-helper.js');
 
+
 let RED;
 module.exports.initRED = function (_RED) {
     RED = _RED;
@@ -11,25 +12,20 @@ module.exports.RedLinkProducer = function (config) {
     RED.nodes.createNode(this, config);
     this.producerStoreName = config.producerStoreName;
     this.producerConsumer  = config.producerConsumer;
-    var node = this;
+    const node = this;
+    const log = require('./log.js')(node).log;
     node.on("input", msg => {
-        msg.msgid = RED.util.generateId();
+        msg.redlinkMsgId = RED.util.generateId();
         const stringify = JSON.stringify(msg);
         const encodedMessage = base64Helper.encode(msg);
         log('the input message is:', stringify);
-        const msgInsertSql = 'INSERT INTO inMessages VALUES ("' + msg.msgid + '","' + this.producerStoreName + '","' + this.producerConsumer + '","' + encodedMessage + '")';
-        log('in the consumer going to execute sql to insert into inmesasges: ', msgInsertSql);
+        const msgInsertSql = 'INSERT INTO inMessages VALUES ("' + msg.redlinkMsgId + '","' + this.producerStoreName + '","' + this.producerConsumer + '","' + encodedMessage + '")';
+        log('in the producer going to execute sql to insert into inmesasges: ', msgInsertSql);
         alasql(msgInsertSql);
+/*
         const allRows = alasql('select * from inMessages');
         log('after inserting input message the inMessages table is:', allRows[0]);
+*/
     });
-    function log() {
-        let i = 0;
-        let str = '';
-        for (; i < arguments.length; i++) {
-            str += ' ' + JSON.stringify(arguments[i], null, 2) + ' ';
-        }
-        node.trace(str);
-}
 
 };
