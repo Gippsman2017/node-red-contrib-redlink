a96f3569b6b9c7e1fbd087899ebfb89b92b98b45
umasudhan <umasudhan.p@gmail.com>
2019-07-22 14:06:01 +1000

    handle dynamic outputs; add missing manual read capability

--- redlinkProducer.js
+++ redlinkProducer.js
@@ -13,43 +13,46 @@ module.exports.RedLinkProducer = function (config) {
     this.producerStoreName = config.producerStoreName;
     this.producerConsumer = config.producerConsumer;
     this.sendOnly = config.sendOnly;
+    this.debug = config.showDebug;
+    this.manualRead = config.manualRead;
     const node = this;
     const log = require('./log.js')(node).log;
 
     const nodeId = config.id.replace('.', '');
     const replyMsgTriggerName = 'replyMessage' + nodeId;
+
+
     alasql.fn[replyMsgTriggerName] = () => {
         //look up inMessages table to see if this producer actually sent the message
         const unreadRepliesSql = 'SELECT redlinkMsgId FROM replyMessages WHERE read=false';
         const unreadReplies = alasql(unreadRepliesSql);
         log('in trigger unread replies:', unreadReplies);
         if (unreadReplies && unreadReplies.length > 0) {
-            let unreadMsgIdsStr =  '(';
-            unreadReplies.forEach(reply=>{
-                unreadMsgIdsStr += '"'+reply.redlinkMsgId+'",'
+            let unreadMsgIdsStr = '(';
+            unreadReplies.forEach(reply => {
+                unreadMsgIdsStr += '"' + reply.redlinkMsgId + '",'
             });
-            unreadMsgIdsStr = unreadMsgIdsStr.substring(0, unreadMsgIdsStr.length - 1) +')';
-            log('unreadMsgIdsStr here 2 :', unreadMsgIdsStr );
-            const msgsByThisProducerSql = 'SELECT * FROM inMessages WHERE redlinkMsgId IN '+unreadMsgIdsStr+' AND producerId="'+node.id+'"';
+            unreadMsgIdsStr = unreadMsgIdsStr.substring(0, unreadMsgIdsStr.length - 1) + ')';
+            log('unreadMsgIdsStr here 2 :', unreadMsgIdsStr);
+            const msgsByThisProducerSql = 'SELECT * FROM inMessages WHERE redlinkMsgId IN ' + unreadMsgIdsStr + ' AND producerId="' + node.id + '"';
             log('msgsByThisProducerSql:', msgsByThisProducerSql);
             const msgsByThisProducer = alasql(msgsByThisProducerSql); //should be length one if reply got for message from this producer else zero
             log('msgsByThisProducer :', msgsByThisProducer);
-            if(msgsByThisProducer && msgsByThisProducer.length>0 ){
+            if (msgsByThisProducer && msgsByThisProducer.length > 0) {
                 const daId = msgsByThisProducer[0].redlinkMsgId;
-                const relevanttReplySql = 'SELECT * FROM replyMessages WHERE redlinkMsgId="'+ daId +'"';
+                const relevanttReplySql = 'SELECT * FROM replyMessages WHERE redlinkMsgId="' + daId + '"';
                 const relevantReplies = alasql(relevanttReplySql); //should have just one result
-                if(relevantReplies && relevantReplies.length >0){
-                    const updateReplySql = 'UPDATE replyMessages SET READ=true WHERE redlinkMsgId="' + daId + '"';
-                    alasql(updateReplySql);
-                    log('replies after updating:', alasql(relevanttReplySql));
-                    const replyMessage = relevantReplies[0].replyMessage;
-                    const reply = {
-                        payload: base64Helper.decode(replyMessage),
-                        redlinkMsgId : daId,
-                        topic: relevantReplies[0].topic
+                if (relevantReplies && relevantReplies.length > 0) {
+                    const notifyMessage = { //todo store more info in replyMesssages- store, etc and populate in notify msg
+                        redlinkMsgId: daId,
+                        notifyType: 'producerReplyNotification',
                     };
-                    log('in producer going to send out reply as:', JSON.stringify(reply, null, 2));
-                    node.send(reply);
+                    if (node.manualRead) {
+                        sendMessage({notify: notifyMessage})
+                    } else {
+                        const reply = getReply(daId, relevanttReplySql, relevantReplies);
+                        sendMessage({receive: reply}, {notify: notifyMessage});
+                    }
                 }
             }
         }
@@ -57,7 +60,65 @@ module.exports.RedLinkProducer = function (config) {
     const createReplyMsgTriggerSql = 'CREATE TRIGGER ' + replyMsgTriggerName + ' AFTER INSERT ON replyMessages CALL ' + replyMsgTriggerName + '()';
     alasql(createReplyMsgTriggerSql);
 
+    function getReply(daId, relevanttReplySql, relevantReplies) {
+        const updateReplySql = 'UPDATE replyMessages SET READ=true WHERE redlinkMsgId="' + daId + '"';
+        alasql(updateReplySql);
+        log('replies after updating:', alasql(relevanttReplySql));
+        const replyMessage = relevantReplies[0].replyMessage;
+        const reply = {
+            payload: base64Helper.decode(replyMessage),
+            redlinkMsgId: daId,
+            topic: relevantReplies[0].topic
+        };
+        log('in producer going to send out reply as:', JSON.stringify(reply, null, 2));
+        return reply;
+    }
+
+    function sendMessage(msg) { //receive, notify, failure, debug
+        const msgs = [];
+        if (!node.sendOnly) { //receive, notify
+            if (msg.receive) {
+                msgs.push(msg.receive);
+            } else {
+                msgs.push(null);
+            }
+            if (msg.notify) {
+                msgs.push(msg.notify);
+            } else {
+                msgs.push(null);
+            }
+        }
+        if (msg.failure) {
+            msgs.push(msg.failure);
+        } else {
+            msgs.push(null);
+        }
+        if (node.debug) {
+            if (msg.debug) {
+                msgs.push(msg.debug);
+            } else {
+                msgs.push(null);
+            }
+        }
+        node.send(msgs);
+    }
+
+
     node.on("input", msg => {
+        if (msg.cmd === 'read') {
+            if (node.manualRead && msg.redlinkMsgId) {
+                const daId = msg.redlinkMsgId;
+                const relevanttReplySql = 'SELECT * FROM replyMessages WHERE redlinkMsgId="' + daId + '"';
+                const relevantReplies = alasql(relevanttReplySql); //should have just one result
+                if (relevantReplies && relevantReplies.length > 0) {
+                    const reply = getReply(daId, relevanttReplySql, relevantReplies);
+                    sendMessage({receive: reply});
+                }
+            } else { //todo error
+
+            }
+            return;
+        }
         msg.redlinkMsgId = RED.util.generateId();
         const stringify = JSON.stringify(msg);
         const encodedMessage = base64Helper.encode(msg);
