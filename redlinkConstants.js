8eb741614aba6ee48d1c347836987787583cf2b9
umasudhan <umasudhan.p@gmail.com>
2020-02-17 11:51:51 +1100

    we pass message metadata in headers- header keys are lowercased by http- we use this constant to fix case at the receiver

---
+++ redlinkConstants.js
@@ -0,0 +1,16 @@
+module.exports.messageFields = { //we pass message metadata in headers- header keys are lowercased by http- we use this constant to fix case at the receiver
+    "redlinkmsgid": "redlinkMsgId",
+    "storename": "storeName",
+    "servicename": "serviceName",
+    "message": "message",
+    "read": "read",
+    "sendonly": "sendOnly",
+    "redlinkproducerid": "redlinkProducerId",
+    "preserved": "preserved",
+    "timestamp": "timestamp",
+    "priority": "priority",
+    "islargemessage": "isLargeMessage",
+    "lifetime": "lifetime",
+    "timesincenotify": "timeSinceNotify",
+    "enforcereversepath": "enforceReversePath"
+};
